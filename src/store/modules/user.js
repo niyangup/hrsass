b291d5d877d563749552ad20c69b15d3c909357b
poper <poper@wangxutech.com>
2022-02-26 17:42:33 +0800

    请求配置

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,6 +1,34 @@
+import { getToken, removeToken, setToken } from '@/utils/auth'
+import { login } from '@/api/user'
+
+const state = {
+  token: getToken()
+}
+
+const mutations = {
+  setToken(state, token) {
+    state.token = token
+    setToken(token)
+  },
+  removeToken(state) {
+    state.token = null
+    removeToken()
+  }
+}
+const actions = {
+  async login(context, data) {
+    const result = await login(data)
+    console.log(result)
+    if (result.data.success) {
+      context.commit('setToken', result.data.data)
+    } else {
+      console.error('登录失败')
+    }
+  }
+}
 export default {
   namespaced: true,
-  state: {},
-  mutations: {},
-  actions: {}
+  state,
+  mutations,
+  actions
 }
