ad6239b78ee30be02177cf0ffe257832a82b9212
poper <poper@wangxutech.com>
2022-02-27 18:05:51 +0800

    头部

--- src/router/moduies/attendances.js
+++ src/router/moduies/attendances.js
@@ -13,5 +13,4 @@ export default {
       icon: 'skill'
     }
   }]
-
 }
--- src/views/departments/index.vue
+++ src/views/departments/index.vue
@@ -1,7 +1,28 @@
 <template>
   <div class="dashboard-container">
     <div class="app-container">
-      <h2>组织</h2>
+      <el-card class="tree-card">
+        <el-row type="flex" align="middle" style="height: 40px;">
+          <el-col>中国村开心快乐哈哈哈哈哈公司</el-col>
+          <el-col :span="4">
+            <el-row type="flex" justify="end">
+              <el-col>负责人</el-col>
+              <el-col>
+                <el-dropdown>
+                  <span>
+                    操作<i class="el-icon-arrow-down el-icon--right"/>
+                  </span>
+
+                  <el-dropdown-menu slot="dropdown">
+                    <el-dropdown-item>添加子部门</el-dropdown-item>
+                  </el-dropdown-menu>
+                </el-dropdown>
+              </el-col>
+            </el-row>
+          </el-col>
+        </el-row>
+
+      </el-card>
     </div>
   </div>
 </template>
@@ -12,6 +33,11 @@ export default {
 }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+
+.tree-card {
+  padding: 30px 140px;
+  font-size: 14px;
+}
 
 </style>
