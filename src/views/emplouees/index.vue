f4a21e10d52a67a04896f2068297ec668b832208
poper <poper@wangxutech.com>
2022-03-01 22:24:30 +0800

    ui完成

--- src/views/emplouees/index.vue
+++ src/views/emplouees/index.vue
@@ -1,14 +1,88 @@
 <template>
   <div class="dashboard-container">
-    <div class="app-container">
-      <h2>员工</h2>
+    <div v-loading="loading" class="app-container">
+      <page-tools :show-before="true">
+
+        <template v-slot:before>
+          <span>共{{ page.total }}条记录</span>
+        </template>
+
+        <template v-slot:after>
+          <el-button size="small" type="success">excel导入</el-button>
+          <el-button size="small" type="danger">excel导出</el-button>
+          <el-button size="small" type="primary">新增员工</el-button>
+        </template>
+
+      </page-tools>
+
+      <el-card>
+        <el-table border :data="list">
+          <el-table-column type="index" label="序号" sortable=""/>
+          <el-table-column label="姓名" prop="username" sortable=""/>
+          <el-table-column label="工号" prop="workNumber" sortable=""/>
+          <el-table-column label="聘用形式" sortable="" prop="formOfEmployment"/>
+          <el-table-column label="部门" sortable="" prop="departmentName"/>
+          <el-table-column label="入职时间" sortable="" prop="timeOfEntry"/>
+          <el-table-column label="账户状态" sortable="" prop="enableState"/>
+          <el-table-column label="操作" sortable="" fixed="right" width="280">
+            <template>
+              <el-button type="text" size="small">查看</el-button>
+              <el-button type="text" size="small">转正</el-button>
+              <el-button type="text" size="small">调岗</el-button>
+              <el-button type="text" size="small">离职</el-button>
+              <el-button type="text" size="small">角色</el-button>
+              <el-button type="text" size="small">删除</el-button>
+            </template>
+          </el-table-column>
+        </el-table>
+        <!-- 分页组件 -->
+        <el-row type="flex" justify="center" align="middle" style="height: 60px">
+          <el-pagination
+            layout="prev, pager, next"
+            :current-page="page.page"
+            :page-size="page.size"
+            :total="page.total"
+            @current-change="changePage"
+          />
+        </el-row>
+      </el-card>
     </div>
   </div>
 </template>
 
 <script>
+import { getEmployeeList } from '@/api/employees'
+
 export default {
-  name: 'Emplouees'
+  name: 'Emplouees',
+  data() {
+    return {
+      list: [],
+      page: {
+        page: 1,
+        size: 10,
+        total: 0
+      },
+      loading: false
+    }
+  },
+  created() {
+    this.getEmployeeList()
+  },
+  methods: {
+    async getEmployeeList() {
+      this.loading = true
+      const { total, rows } = await getEmployeeList(this.page)
+      this.page.total = total
+      this.list = rows
+      this.loading = false
+    },
+    changePage(newPage) {
+      this.page.page = newPage
+      this.getEmployeeList()
+    }
+  }
+
 }
 </script>
 
